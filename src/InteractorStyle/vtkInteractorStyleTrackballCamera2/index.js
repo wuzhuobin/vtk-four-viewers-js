55f383a025470fe1e710e6e146979b8ef89beaa6
gundam <gundam@lenovo>
2018-12-23 20:24:00 +0800

    vtkInteractorStyleTrackballCamera2 update mouse wheel to slicing

--- src/InteractorStyle/vtkInteractorStyleTrackballCamera2/index.js
+++ src/InteractorStyle/vtkInteractorStyleTrackballCamera2/index.js
@@ -1,5 +1,11 @@
+// vtk
 import macro from 'vtk.js/Sources/macro';
+import {States} from 'vtk.js/Sources/Rendering/Core/InteractorStyle/Constants';
 import vtkInteractorStyleTrackballCamera from 'vtk.js/Sources/Interaction/Style/InteractorStyleTrackballCamera';
+
+// ----------------------------------------------------------------------------
+//  vtkInteractorStyleTrackballCamera2 methods
+// ----------------------------------------------------------------------------
 
 function vtkInteractorStyleTrackballCamera2(publicAPI, model){
   // Set our className
@@ -9,25 +15,66 @@ function vtkInteractorStyleTrackballCamera2(publicAPI, model){
   const superClass = Object.assign({}, publicAPI);
 
   // Public API methods
-
-  publicAPI.handleKeyPress = (callData) =>{
-    const interactor = model.interactor;
+  publicAPI.handleKeyPress = (callData) => {
     switch (callData.key){
       case 'r':
       case 'R':
-        if (model.reactVtkViewer === null) {
+        if (model.viewer === null) {
           superClass.handleKeyPress(callData);
         }
-        else {
-          model.reactVtkViewer.renderer.resetCamera();
-          // model.reactVtkVie 
+        else if (model.viewer.isA('vtkImageViewer')) {
+          model.viewer.updateDisplayExtent();
+          model.viewer.updateOrientation();
+          model.viewer.getRenderer().resetCamera();
+          model.viewer.getRenderer().resetCameraClippingRange();
+          model.viewer.getRenderWindow().render();
         }
-
         break;
-
       default:
         superClass.handleKeyPress(callData);
         break;
+    }
+  };
+
+  //--------------------------------------------------------------------------
+  publicAPI.handleStartMouseWheel = (callData) => {
+    if(model.viewer === null ) {
+      superClass.handleStartMouseWheel();
+    }
+    else if (model.viewer.isA('vtkImageViewer')){
+      publicAPI.startSlice();
+    }
+  };
+
+  //--------------------------------------------------------------------------
+  publicAPI.handleEndMouseWheel = () => {
+    switch (model.state) {
+      case States.IS_SLICE:
+        publicAPI.endSlice();
+        break;
+      default: 
+        superClass.handleEndMouseWheel();
+        break;
+    }
+  };
+
+  //--------------------------------------------------------------------------
+  publicAPI.handleMouseWheel = (callData) => {
+    switch (model.state){
+      case States.IS_SLICE:
+        publicAPI.slice(callData.spinY);
+        break;
+      default:
+        publicAPI.superHandleMouseWheel(callData);
+    }
+  };
+
+  //--------------------------------------------------------------------------
+  publicAPI.slice = (step) => {
+    if(model.viewer !== null && model.viewer.isA('vtkImageViewer')) {
+      const slice = model.viewer.getSlice();
+      model.viewer.setSlice(slice + step);
+      publicAPI.invokeInteractionEvent({ type: 'InteractionEvent' });
     }
   }
 }
@@ -37,7 +84,7 @@ function vtkInteractorStyleTrackballCamera2(publicAPI, model){
 // ----------------------------------------------------------------------------
 
 const DEFAULT_VALUES = {
-  reactVtkViewer: null,
+  viewer: null,
 };
 
 // ----------------------------------------------------------------------------
@@ -49,7 +96,7 @@ export function extend(publicAPI, model, initialValues = {}) {
   vtkInteractorStyleTrackballCamera.extend(publicAPI, model, initialValues);
 
   // Create get-set macros
-  macro.setGet(publicAPI, model, ['reactVtkViewer']);
+  macro.setGet(publicAPI, model, ['viewer']);
 
   // Object specific methods
   vtkInteractorStyleTrackballCamera2(publicAPI, model);
@@ -57,10 +104,7 @@ export function extend(publicAPI, model, initialValues = {}) {
 
 // ----------------------------------------------------------------------------
 
-export const newInstance = macro.newInstance(
-  extend,
-  'vtkInteractorStyleTrackballCamera'
-)
+export const newInstance = macro.newInstance(extend, 'vtkInteractorStyleTrackballCamera2');
 
 // ----------------------------------------------------------------------------
 
